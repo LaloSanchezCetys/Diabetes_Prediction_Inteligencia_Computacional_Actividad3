93aece2d866a8aef7ff3e10236ccee9cbf9925b1
Eduardo Sánchez <eduardo.sanchez@cetys.edu.mx>
2026-02-23 20:14:14 -0800

    Fix static assets for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,17 @@
 const nextConfig = {
   output: "export",
 
-  // Required for GitHub Pages (repo name goes here)
   basePath: "/Diabetes_Prediction_Inteligencia_Computacional_Actividad3",
   assetPrefix: "/Diabetes_Prediction_Inteligencia_Computacional_Actividad3/",
 
-  // Your existing config
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  trailingSlash: true,
+
   images: {
     unoptimized: true,
+  },
+
+  typescript: {
+    ignoreBuildErrors: true,
   },
 };
 
