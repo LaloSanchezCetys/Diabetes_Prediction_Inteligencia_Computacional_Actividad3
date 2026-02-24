5a75e27845d3fdec9b8421bf6d7b16c827d0652a
Eduardo Sánchez <eduardo.sanchez@cetys.edu.mx>
2026-02-23 20:04:48 -0800

    Add static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
+  // Required for GitHub Pages (repo name goes here)
+  basePath: "/Diabetes_Prediction_Inteligencia_Computacional_Actividad3",
+  assetPrefix: "/Diabetes_Prediction_Inteligencia_Computacional_Actividad3/",
+
+  // Your existing config
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
---
+++ out/_next/static/58JaGfhIr0JFHwpXmxg6k/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Diabetes_Prediction_Inteligencia_Computacional_Actividad3//_next/:path+",
+        "destination": "/Diabetes_Prediction_Inteligencia_Computacional_Actividad3/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
